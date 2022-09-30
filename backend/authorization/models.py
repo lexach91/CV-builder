3cb76e95b35f11a736457577c6e329525d693b39
lexach91 <ronmctailor@protonmail.com>
2022-09-30 17:29:22 +0800

    Add custom user model and user manager

--- backend/authorization/models.py
+++ backend/authorization/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self,
+        email,
+        password=None,
+        first_name=None,
+        last_name=None,
+        birthday=None,
+        country=None,
+    ):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            birthday=birthday,
+            country=country,
+        )
+
+        user.set_password(password)
+        user.is_superuser = False
+        user.is_staff = False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
