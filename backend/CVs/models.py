3e95e4879e7295397ddbcafc66ab8f1dc215f9c2
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-05 13:29:06 +0800

    Add cv experience related models

--- backend/CVs/models.py
+++ backend/CVs/models.py
@@ -68,3 +68,82 @@ class Summary(models.Model):
     class Meta:
         verbose_name_plural = "Summaries"
 
+
+class ExperienceSection(models.Model):
+    """ExperienceSection model"""
+    cv = models.ForeignKey(CV, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.cv
+
+    class Meta:
+        verbose_name_plural = "ExperienceSections"
+
+    class Meta:
+        verbose_name_plural = "ExperienceSections"
+
+
+class Experience(models.Model):
+    """Experience model"""
+    experience_section = models.ForeignKey(
+        ExperienceSection,
+        on_delete=models.CASCADE,
+        related_name="experiences"
+    )
+    company = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True,
+        unique=False
+    )
+    position = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True,
+        unique=False
+    )
+    start_date = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True,
+        unique=False
+    )
+    end_date = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True,
+        unique=False
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        unique=False
+    )
+
+    def __str__(self):
+        return self.company
+
+    class Meta:
+        verbose_name_plural = "Experiences"
+
+
+class ExperienceBullet(models.Model):
+    """Experience bullet model"""
+    experience = models.ForeignKey(
+        Experience,
+        on_delete=models.CASCADE,
+        related_name="experience_bullets"
+    )
+    bullet = models.TextField(
+        blank=True,
+        null=True,
+        unique=False
+    )
+
+    def __str__(self):
+        return self.bullet
+
+    class Meta:
+        verbose_name_plural = "Job bullets"
+
+
