2615b8fe6511dd8842b3db82443d13173d99abb7
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-05 13:15:50 +0800

    Add core cv model to CVs app

--- backend/CVs/models.py
+++ backend/CVs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from authorization.models import User
 
-# Create your models here.
+class CV(models.Model):
+    """CV model"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
