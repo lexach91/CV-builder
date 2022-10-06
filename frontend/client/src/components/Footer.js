bbaab573fbac59a639176b677794b9b3fea1e7ab
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-06 20:23:48 +0800

    Add core footer to the app

---
+++ frontend/client/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer bg-red-300">
+      <div className="content has-text-centered fixed-bottom">
+        footer
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
