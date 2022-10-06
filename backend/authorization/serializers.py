c7b758007b1f46a9cbb4f03400fffd583b63d1fa
lexach91 <ronmctailor@protonmail.com>
2022-10-06 17:24:51 +0800

    Add serializer for creating a user

---
+++ backend/authorization/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+
+class CreateUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "password",
+            "first_name",  # optional
+            "last_name",  # optional
+            "birthday",  # optional
+            "country",  # optional
+        )
+        extra_kwargs = {
+           "password": {"write_only": True},
+           "first_name": {"required": False},
+           "last_name": {"required": False},
+           "birthday": {"required": False},
+           "country": {"required": False},
+        }
+        
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
