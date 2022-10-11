79fed5d6bdf257af4adb5f71d372a491db91d287
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-11 17:22:08 +0800

    Add import of handlers from  user.js

--- frontend/client/src/store.js
+++ frontend/client/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from 'features/user';
+import userReducer from './features/user';
 
 export const store = configureStore({
 	reducer: {
