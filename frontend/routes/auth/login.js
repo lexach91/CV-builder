80d29d28be1ef13aac765b197000e00f6487200d
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-14 18:45:39 +0800

    Add login route to express

---
+++ frontend/routes/auth/login.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cookie = require('cookie');
+const fetch = (...args) =>
+	import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const router = express.Router();
+
+router.post('api/auth/login'), async(req,res) => {
+  const {email, password} = req.body;
+
+  const body = JSON.stringify({email, password});
+
+  try {
+    const loginResponse = await fetch(`${process.env.API_URL}/auth/`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body,
+    });
+    const data = await loginResponse.json();
+
+    if (loginResponse.status === 200) {
+      res.setHeader(
+        'Set-Cookie', [
+        cookie.serialize('access', data.access, {
+          httpOnly: true,
+          maxAge: 60 * 30,
+          path: '/api/',
+          sameSite: 'strict',
+          secure: process.env.NODE_ENV === 'production',
+        }),
+        cookie.serialize('refresh', data.refresh, {
+          httpOnly: true,
+          maxAge: 60 * 60 * 24,
+          path: '/api/',
+          sameSite: 'strict',
+          secure: process.env.NODE_ENV === 'production',
+        }),
+      ]);
+      return res.status(loginResponse.status).json({ access: 'login successful' });
+    } else {
+      return res.status(loginResponse.status).json(data);
+    }
+  } catch(error) {
+    console.log(error.message);
+    return res.status(500).json({
+      error: "Something went wrong when logging in"
+    })
+  }
+
+
+}
+
+module.exports = router;
