500a9ecb829df78b074505e1a002706687535c93
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-16 17:41:09 +0800

    Fix submit button for login form

--- frontend/client/src/containers/LoginPage.js
+++ frontend/client/src/containers/LoginPage.js
@@ -79,9 +79,8 @@ const LoginPage = () => {
           <div className="flex w-full justify-center">
             <button
               className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
-              type="button"
                   >
-              Register
+              Login
             </button>
           </div>
         </form>
