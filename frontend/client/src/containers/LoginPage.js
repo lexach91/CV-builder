2f1f2bf38bf887f7f2c335c957660ba98ee07002
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-24 17:55:58 +0800

    Change login form with react-final-form

--- frontend/client/src/containers/LoginPage.js
+++ frontend/client/src/containers/LoginPage.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import Layout from "../components/Layout";
 import { Link, Navigate } from "react-router-dom";
+import { Form, Field } from 'react-final-form';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { Divider } from 'primereact/divider';
+import { Password } from 'primereact/password';
+import { classNames } from 'primereact/utils';
 import { login } from '../features/user'
 
 // Set up error messages
@@ -12,21 +19,58 @@ const LoginPage = () => {
   const { loading, isAuthenticated, registered } = useSelector(
 		state => state.user
 	);
+  const [showMessage, setShowMessage] = useState(false);
+  const [formData, setFormData] = useState({});
 
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
 
-  const { email, password } = formData;
-  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const validate = (data) => {
+    let errors = {};
 
-  const onSubmit = e => {
-		e.preventDefault();
+    if (!data.email) {
 
-		dispatch(login({ email, password }));
-    console.log(formData);
-	};
+        errors.email = 'Email is required.';
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
+      errors.email = 'Invalid email address. E.g. example@email.com';
+  }
+
+    if (!data.password) {
+        errors.password = 'Password is required.';
+    }
+
+    return errors;
+  }
+
+  const onSubmit = (data, form) => {
+    setFormData(data);
+    const payload = {
+      email: data.email,
+      password: data.password
+    }
+    console.log(payload);
+    dispatch(login(payload));
+  }
+
+  const isFormFieldValid = (meta) => !!(meta.touched && meta.error);
+  const getFormErrorMessage = (meta) => {
+      return isFormFieldValid(meta) && <small className="p-error">{meta.error}</small>;
+  };
+
+
+  const dialogFooter = <div className="flex justify-content-center"><Button label="OK" className="p-button-text" autoFocus onClick={() => setShowMessage(false) } /></div>;
+  const passwordHeader = <h6>Pick a password</h6>;
+  const passwordFooter = (
+      <React.Fragment>
+          <Divider />
+          <p className="mt-2">Suggestions</p>
+          <ul className="pl-2 ml-2 mt-0" style={{ lineHeight: '1.5' }}>
+              <li>At least one lowercase</li>
+              <li>At least one uppercase</li>
+              <li>At least one numeric</li>
+              <li>Minimum 8 characters</li>
+          </ul>
+      </React.Fragment>
+  );
 
   if (isAuthenticated) {
     return <Navigate to='/cvs' />;
@@ -34,58 +78,51 @@ const LoginPage = () => {
 
   return (
     <Layout title='CV-builder | Register' content='Register to the app'>
-      <div className='container flex flex-wrap justify-center flex-col content-center h-[90%] my-20 bg-slate-800 mx-auto max-w-120rem'>
-        <form
-          className="w-full max-w-lg bg-slate-900 py-9 px-10 rounded-xl"
-          onSubmit={onSubmit}
-        >
-          <h1 className='text-5xl text-center mb-6 text-emerald-400'>Login</h1>
-          <p className='text-xl text-center'>
-            Don't have an account? <Link className="ml-2 underline text-emerald-400 hover:text-emerald-600" to="/register">Register</Link>
-          </p>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className="w-full px-3">
-              <label
-                htmlFor="email"
-                className="block uppercase tracking-wide text-slate-300 text-xs font-bold mb-2"
+      <div className='form-demo'>
+        <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
+          <div className="flex align-items-center flex-column pt-6 px-3">
+            <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
+            <h5>Registration Successful!</h5>
+            <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
+              Your account is registered under name <b>{formData.name}</b> ; it'll be valid next 30 days without activation. Please check <b>{formData.email}</b> for activation instructions.
+            </p>
+          </div>
+        </Dialog>
+        <div className="flex justify-content-center">
+          <div className="card">
+            <Form onSubmit={onSubmit} initialValues={{ email: '', password: ''}} validate={validate} render={({ handleSubmit }) => (
+              <form
+                className="p-fluid bg-slate-900 p-6 rounded shadow-md"
+                onSubmit={handleSubmit}
               >
-                Email
-              </label>
-              <input
-                id="email"
-                onChange={onChange}
-                value={email}
-                name="email"
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                placeholder='Enter email' />
-            </div>
+                <h1 className='text-5xl text-center mb-6 text-emerald-400'>Register</h1>
+                <p className='text-xl text-center'>
+                  Already have an account? <Link className="ml-2 underline text-emerald-400 hover:text-emerald-600" to="/login">Login</Link>
+                </p>
+                <Field name="email" render={({ input, meta }) => (
+                  <div className="field">
+                    <span className="p-float-label p-input-icon-right">
+                      <i className="pi pi-envelope" />
+                      <InputText id="email" {...input} className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
+                      <label htmlFor="email" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Email*</label>
+                    </span>
+                    {getFormErrorMessage(meta)}
+                  </div>
+                )} />
+                <Field name="password" render={({ input, meta }) => (
+                  <div className="field">
+                    <span className="p-float-label">
+                      <Password id="password" {...input} toggleMask className={classNames({ 'p-invalid': isFormFieldValid(meta) })} header={passwordHeader} footer={passwordFooter} />
+                      <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Password*</label>
+                    </span>
+                    {getFormErrorMessage(meta)}
+                  </div>
+                )} />
+                <Button type="submit" label="Submit" className="mt-2" />
+              </form>
+            )} />
           </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-slate-300 text-xs font-bold mb-2"
-                htmlFor="grid-password">
-                  Password
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="password"
-                name="password"
-                onChange={onChange}
-                value={password}
-                type="password"
-                placeholder="******************"/>
-              <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
-            </div>
-          </div>
-          <div className="flex w-full justify-center">
-            <button
-              className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
-                  >
-              Login
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </Layout>
   );
