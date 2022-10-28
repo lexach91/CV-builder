b8a599f1daa52d0bdd315b707f0e1f3a4d9514e9
lexach91 <ronmctailor@protonmail.com>
2022-10-28 19:39:25 +0800

    Add api views for all and individual CV

--- backend/CVs/views.py
+++ backend/CVs/views.py
@@ -29,3 +29,96 @@ from .models import (
     InterestSection,
     Interest,
     )
+from .serializers import (
+    CVSerializer,
+    HeaderSerializer,
+    SummarySerializer,
+    ExperienceBulletSerializer,
+    ExperienceSerializer,
+    ExperienceSectionSerializer,
+    EducationBulletSerializer,
+    EducationSerializer,
+    EducationSectionSerializer,
+    ShowcaseBulletSerializer,
+    ShowcaseSerializer,
+    ShowcaseSectionSerializer,
+    SoftSkillSerializer,
+    SoftSkillSectionSerializer,
+    ProfessionalSkillSerializer,
+    ProfessionalSkillSectionSerializer,
+    SocialLinkSerializer,
+    SocialLinkSectionSerializer,
+    LanguageSerializer,
+    LanguageSectionSerializer,
+    VolunteerBulletSerializer,
+    VolunteerSerializer,
+    VolunteerSectionSerializer,
+    InterestSerializer,
+    InterestSectionSerializer,
+    )
+
+
+class MyCVsAPIView(APIView):
+    def get(self, request):
+        user = request.user
+        cvs = CV.objects.filter(user=user)
+        if not cvs:
+            return Response(
+                {"error": "No CVs found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = CVSerializer(cvs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def post(self, request):
+        user = request.user
+        serializer = CVSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class CVDetailsAPIView(APIView):
+    def get(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = CVSerializer(cv)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def put(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = CVSerializer(cv, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def delete(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        cv.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
