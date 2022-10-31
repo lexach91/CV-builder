eae02a1f3dc22199096fac29337ac63527f90f36
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-31 18:23:09 +0800

    Add basic form for editing profile

--- frontend/client/src/containers/ProfilePage.js
+++ frontend/client/src/containers/ProfilePage.js
@@ -3,14 +3,26 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { CountryService } from "../service/CountryService";
+import { Form, Field } from 'react-final-form';
+import { Calendar } from 'primereact/calendar';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { Divider } from 'primereact/divider';
+import { Password } from 'primereact/password';
+import { classNames } from 'primereact/utils';
+import { resetRegistered } from '../features/user';
 
 
 const ProfilePage = () => {
   const { isAuthenticated, user, registered, loading } = useSelector(
-    (state) => state.auth
+    (state) => state.user
   );
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -23,7 +35,6 @@ const ProfilePage = () => {
   const [editLastName, setEditLastName] = useState(false);
   const [editBirthDate, setEditBirthDate] = useState(false);
   const [editCountry, setEditCountry] = useState(false);
-  // const toast = useRef(null);
   const [formUploading, setFormUploading] = useState(false);
   const [dialogVisible, setDialogVisible] = useState(false);
   const [passwordUpdating, setPasswordUpdating] = useState(false);
@@ -64,7 +75,6 @@ const ProfilePage = () => {
     }
   }, [isAuthenticated, user]);
 
-
   return (
     <Layout title='CV-builder | My CVs' content='Welcome to the Your Profile page'>
       <div className="pt-4 container mx-auto">
@@ -74,6 +84,211 @@ const ProfilePage = () => {
         <p  className="text-xl text-center">
 
         </p>
+
+      <Dialog
+        header="Change Password"
+        footer={
+          <div>
+            <Button
+              label="Cancel"
+              onClick={() => {
+                setDialogVisible(false);
+                setOldPassword("");
+                setNewPassword("");
+                setConfirmPassword("");
+              }}
+            />
+            <Button
+              label="Submit"
+              disabled={
+                oldPassword.length === 0 ||
+                newPassword.length === 0 ||
+                confirmPassword.length === 0 ||
+                newPassword !== confirmPassword
+              }
+              onClick={() => {
+
+              }}
+            />
+          </div>
+        }>
+        <div className="form-group">
+          <label htmlFor="oldPassword">Old Password</label>
+          <Password
+            id="oldPassword"
+            value={oldPassword}
+            // onChange={}
+            placeholder="Old Password"
+            feedback={false}
+            toggleMask={true}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="newPassword">New Password</label>
+          <Password
+            id="newPassword"
+            value={newPassword}
+            // onChange={}
+            placeholder="New Password"
+            toggleMask={true}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <Password
+            id="confirmPassword"
+            value={confirmPassword}
+            // onChange={}
+            placeholder="Confirm Password"
+            feedback={false}
+            toggleMask={true}
+          />
+        </div>
+      </Dialog>
+
+
+      <div className="surface-0 p-4 w-full md:w-8 md:mx-auto">
+        <div className="text-500 mb-3">
+          Your profile information can be changed here.
+        </div>
+        <ul className="list-none p-0 m-0">
+          <li className="flex align-items-center py-3 px-2 border-top-1 border-300 flex-wrap">
+            <div className="text-500 w-6 md:w-2 font-medium">First Name</div>
+            <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1">
+              {editFirstName ? (
+                <InputText
+                  name="first_name"
+                  value={firstName}
+                  // onChange={}
+                  // onBlur={}
+                />
+              ) : (
+                <div className="text-900">{firstName}</div>
+              )}
+            </div>
+            <div className="w-6 md:w-2 flex justify-content-end">
+              <Button
+                label="Edit"
+                icon="pi pi-pencil"
+                className="p-button-text"
+                // onClick={}
+              />
+            </div>
+          </li>
+          <li className="flex align-items-center py-3 px-2 border-top-1 border-300 flex-wrap">
+            <div className="text-500 w-6 md:w-2 font-medium">Last Name</div>
+            <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1">
+              {editLastName ? (
+                <InputText
+                  name="last_name"
+                  value={lastName}
+                  // onChange={}
+                  // onBlur={}
+                />
+              ) : (
+                <div className="text-900">{lastName}</div>
+              )}
+            </div>
+            <div className="w-6 md:w-2 flex justify-content-end">
+              <Button
+                label="Edit"
+                icon="pi pi-pencil"
+                className="p-button-text"
+                onClick={() => setEditLastName(!editLastName)}
+              />
+            </div>
+          </li>
+          <li className="flex align-items-center py-3 px-2 border-top-1 border-300 flex-wrap">
+            <div className="text-500 w-6 md:w-2 font-medium">Email</div>
+            <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1">
+              <div className="text-900">{email}</div>
+            </div>
+            <div className="w-6 md:w-2 flex justify-content-end">
+              <Button
+                label="Can't change"
+                icon="pi pi-times"
+                className="p-button-text"
+                disabled={true}
+              />
+            </div>
+          </li>
+          <li className="flex align-items-center py-3 px-2 border-top-1 border-bottom-1 border-300 flex-wrap">
+            <div className="text-500 w-6 md:w-2 font-medium">Birth Date</div>
+            <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1 line-height-3">
+              {editBirthDate ? (
+                <Calendar
+                  name="birth_date"
+                  dateFormat="yy-mm-dd"
+                  mask="9999-99-99"
+                  showIcon
+                  value={new Date(birthDate)}
+                  // onChange={}
+                />
+              ) : (
+                <div className="text-900">{birthDate}</div>
+              )}
+            </div>
+            <div className="w-6 md:w-2 flex justify-content-end">
+              <Button
+                label="Edit"
+                icon="pi pi-pencil"
+                className="p-button-text"
+                // onClick={}
+              />
+            </div>
+          </li>
+          <li className="flex align-items-center py-3 px-2 border-top-1 border-bottom-1 border-300 flex-wrap">
+            <div className="text-500 w-6 md:w-2 font-medium">Country</div>
+            <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1 line-height-3">
+              {editCountry ? (
+                <Dropdown
+                  name="country"
+                  value={country}
+                  options={countriesList}
+                  // onChange={}
+                />
+              ) : (
+                <div className="text-900">{country}</div>
+              )}
+            </div>
+            <div className="w-6 md:w-2 flex justify-content-end">
+              <Button
+                label="Edit"
+                icon="pi pi-pencil"
+                className="p-button-text"
+                // onClick={}
+              />
+            </div>
+          </li>
+          <li className="flex align-items-center py-3 px-2 border-top-1 border-bottom-1 border-300 flex-wrap">
+            <div className="text-500 w-6 md:w-2 font-medium">Password</div>
+            <div className="text-900 w-full md:w-8 md:flex-order-0 flex-order-1 line-height-3">
+              <div className="text-900">*********</div>
+              {}
+            </div>
+            <div className="w-6 md:w-2 flex justify-content-end">
+              <Button
+                label="Edit"
+                icon="pi pi-pencil"
+                className="p-button-text"
+                // onClick={}
+              />
+            </div>
+          </li>
+        </ul>
+        <div className="flex justify-content-center mt-3">
+          <Button
+            label="Save"
+            icon="pi pi-check"
+            className="p-button-primary"
+            // onClick={}
+            loading={formUploading}
+            // disabled={}
+          />
+        </div>
+      </div>
+  
+    <> </>
       </div>
     </Layout>
   );
