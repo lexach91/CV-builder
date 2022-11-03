0d6258183b31721d3c05ecdb1a4fee971fb45536
lexach91 <ronmctailor@protonmail.com>
2022-11-03 12:18:29 +0800

    Add code to verify token in verify.js route

--- frontend/routes/auth/verify.js
+++ frontend/routes/auth/verify.js
@@ -3,32 +3,34 @@ const fetch = (...args) =>
 	import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const router = express.Router();
+console.log('in verify route');
 
 // for the endpoint on express
 router.get('/api/auth/verify', async (req, res) => {
   // parse access token from cookie
 	const { access } = req.cookies;
 
-  const body = JSON.stringify({
-    token: access,
-  });
+//   const body = JSON.stringify({
+//     token: access,
+//   });
 
 	try {
-		const apiRes = await fetch(`${process.env.API_URL}/api/auth/verify/`, {
-			method: 'POST',
+		const apiRes = await fetch(`${process.env.API_URL}/api/auth/verify`, {
+			method: 'GET',
 			headers: {
 				Accept: 'application/json',
-				'Content-Type': 'application/json',
+				Authorization: `Bearer ${access}`,
 			},
-      body,
 		});
+		// in return we get only a response code
+		return res.status(apiRes.status).json({});
 
-		const data = await apiRes.json();
+		// const data = await apiRes.json();
 
-		return res.status(apiRes.status).json(data);
+		// return res.status(apiRes.status).json(data);
 	} catch (err) {
 		return res.status(500).json({
-			error: 'Something went wrong when trying to retrieve user',
+			error: 'Something went wrong when verifying token',
 		});
 	}
 });
