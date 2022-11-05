7ab7fb759339e991ff4c3e2085f5eebd611e36d5
lexach91 <ronmctailor@protonmail.com>
2022-11-05 19:15:26 +0800

    Add CVs app models to admin panel

--- backend/CVs/admin.py
+++ backend/CVs/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import (
+    Theme,
+    CV,
+    Header,
+    Summary,
+    ExperienceSection,
+    Experience,
+    ExperienceBullet,
+    EducationSection,
+    Education,
+    EducationBullet,
+    ShowcaseSection,
+    Showcase,
+    ShowcaseBullet,
+    SoftSkillSection,
+    SoftSkill,
+    ProfessionalSkillSection,
+    ProfessionalSkill,
+    SocialLinkSection,
+    SocialLink,
+    LanguageSection,
+    Language,
+    VolunteerSection,
+    Volunteer,
+    VolunteerBullet,
+    InterestSection,
+    Interest,
+    )
 
-# Register your models here.
+
+admin.site.register(Theme)
+admin.site.register(CV)
+admin.site.register(Header)
+admin.site.register(Summary)
+admin.site.register(ExperienceSection)
+admin.site.register(Experience)
+admin.site.register(ExperienceBullet)
+admin.site.register(EducationSection)
+admin.site.register(Education)
+admin.site.register(EducationBullet)
+admin.site.register(ShowcaseSection)
+admin.site.register(Showcase)
+admin.site.register(ShowcaseBullet)
+admin.site.register(SoftSkillSection)
+admin.site.register(SoftSkill)
+admin.site.register(ProfessionalSkillSection)
+admin.site.register(ProfessionalSkill)
+admin.site.register(SocialLinkSection)
+admin.site.register(SocialLink)
+admin.site.register(LanguageSection)
+admin.site.register(Language)
+admin.site.register(VolunteerSection)
+admin.site.register(Volunteer)
+admin.site.register(VolunteerBullet)
+admin.site.register(InterestSection)
+admin.site.register(Interest)
