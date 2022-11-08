f906e8272c6c37f53775a4d880ec4ca7dbb10124
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-11-08 15:12:11 +0800

    Add initial form for cvs

--- frontend/client/src/containers/CreateCVPage.js
+++ frontend/client/src/containers/CreateCVPage.js
@@ -1,4 +1,14 @@
 import Layout from "../components/Layout";
+import React, { useEffect, useState } from "react";
+import { Form, Field } from 'react-final-form';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
+
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 
 const CreateCVPage = () => {
@@ -11,6 +21,57 @@ const CreateCVPage = () => {
         <p  className="text-xl text-center">
           Here goes the create CV Page
         </p>
+        <div className="flex justify-content-center">
+          <div className="card">
+          <Form onSubmit={onSubmit}
+            initialValues={{ first_name: '', last_name: '', email: '', password: '', birthday: null, country: null, password_confirm: '' }}
+            // validate={validate}
+            render={({ handleSubmit }) => (
+              <form
+                className="p-fluid bg-slate-900 p-6 rounded shadow-md"
+              >
+                <h1 className='text-5xl text-center mb-6 text-emerald-400'>Create CV</h1>
+
+                <Field name="first_name" render={({ input, meta }) => (
+                  <div className="field">
+                    <span className="p-float-label">
+                      <InputText
+                        id="first_name" {...input}
+                        autoFocus
+                        className=""
+                      />
+                      <label
+                        htmlFor="first_name"
+                        className="">
+                          First name*
+                      </label>
+                    </span>
+
+                  </div>
+                )} />
+                <Field name="last_name" render={({ input, meta }) => (
+                  <div className="field">
+                    <span className="p-float-label">
+                      <InputText
+                        id="last_name" {...input}
+                        autoFocus
+                        className=""
+                      />
+                      <label
+                        htmlFor="last_name"
+                        className="">
+                          First name*
+                      </label>
+                    </span>
+
+                  </div>
+                )} />
+                                <Button type="submit" label="Submit" className="mt-2" />
+              </form>
+              )} />
+          </div>
+        </div>
+
       </div>
     </Layout>
   );
