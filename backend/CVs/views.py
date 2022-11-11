28a9730af9afc6307b93c2772c97690c2ce6220d
lexach91 <ronmctailor@protonmail.com>
2022-11-11 17:10:40 +0800

    Add api view to create, edit, and delete CV's education section

--- backend/CVs/views.py
+++ backend/CVs/views.py
@@ -289,3 +289,61 @@ class ExperienceSectionAPIView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
 
+class EducationSectionAPIView(APIView):
+    """Create and edit education section of CV"""
+    authentication_classes = [JWTAuthentication]
+    def post(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = EducationSectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(cv=cv)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        try:
+            education_section = EducationSection.objects.get(cv=cv)
+        except EducationSection.DoesNotExist:
+            return Response(
+                {"error": "Education section not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = EducationSectionSerializer(education_section, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        try:
+            education_section = EducationSection.objects.get(cv=cv)
+        except EducationSection.DoesNotExist:
+            return Response(
+                {"error": "Education section not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        education_section.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
