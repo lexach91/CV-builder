f9f20622dbf0933ddbcca09950a14c62fb3f6c3a
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-11-11 16:10:17 +0800

    Add navLink to create CV page

--- frontend/client/src/containers/MyCVsPage.js
+++ frontend/client/src/containers/MyCVsPage.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import { NavLink } from "react-router-dom";
 
 
 const MyCVsPage = () => {
@@ -10,6 +11,15 @@ const MyCVsPage = () => {
         </h1>
         <p  className="text-xl text-center">
           Here goes the CVs Page
+        </p>
+        <div className="flex justify-content-center m-4">
+          <NavLink to="/create-cv"
+            className="p-button-info flex align-items-center justify-content-center font-bold text-white border-round capitalize bg-purple-400 hover:bg-purple-600 max-w-fit px-4 py-2">
+            Create New CV
+          </NavLink>
+        </div>
+        <p  className="text-xl text-center">
+          Here you will find your CVs created with CV-builder before
         </p>
       </div>
     </Layout>
