851061b0858812ba8728cf25e26218527d670988
lexach91 <ronmctailor@protonmail.com>
2022-11-11 17:16:22 +0800

    Add api view to create, edit, and delete CV's interest section

--- backend/CVs/views.py
+++ backend/CVs/views.py
@@ -708,3 +708,63 @@ class VolunteerSectionAPIView(APIView):
             )
         volunteer_section.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class InterestSectionAPIView(APIView):
+    """Create and edit interest section of CV"""
+    authentication_classes = [JWTAuthentication]
+    def post(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = InterestSectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(cv=cv)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        try:
+            interest_section = InterestSection.objects.get(cv=cv)
+        except InterestSection.DoesNotExist:
+            return Response(
+                {"error": "Interest section not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = InterestSectionSerializer(interest_section, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        user = request.user
+        try:
+            cv = CV.objects.get(pk=pk, user=user)
+        except CV.DoesNotExist:
+            return Response(
+                {"error": "CV not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        try:
+            interest_section = InterestSection.objects.get(cv=cv)
+        except InterestSection.DoesNotExist:
+            return Response(
+                {"error": "Interest section not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        interest_section.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
