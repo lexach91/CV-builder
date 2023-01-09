85e1ad1b8ddc36ad53a2779c3061e813c5cb24f0
lexach91 <ronmctailor@protonmail.com>
2023-01-09 15:54:46 +0700

    Add base url to api/axios.js

--- frontend/client/src/api/axios.js
+++ frontend/client/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = window.location.origin;
 
 axios.defaults.baseURL = baseURL + "/api/";
 axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
+
+export default axios;
