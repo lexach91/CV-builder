dc8a15ab9ade24ced761f066703d12b9a77ec7d8
lexach91 <ronmctailor@protonmail.com>
2023-01-09 18:33:48 +0700

    Add axios interceptor to refresh a token

--- frontend/client/src/api/axios.js
+++ frontend/client/src/api/axios.js
@@ -7,4 +7,34 @@ axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.withCredentials = true;
 
-export default axios;
+let refreshing = false;
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            if (refreshing) {
+                return;
+            }
+            refreshing = true;
+            return axios
+                .post("auth/refresh", {
+                    withCredentials: true,
+                })
+                .then((response) => {
+                    // refreshing = false;
+                    return axios(error.config);
+                })
+                .catch((err) => {
+                    refreshing = false;
+                    return Promise.reject("refresh failed");
+                });
+        } else {
+            return Promise.reject("something went wrong");
+        }
+
+    }
+);
+
+export default baseURL;
