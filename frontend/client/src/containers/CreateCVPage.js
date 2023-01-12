5cf54a01f69ab130894630daf8759f4af14ea02b
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-12 14:14:56 +0700

    Fix code flow in CreateCVPage.js

--- frontend/client/src/containers/CreateCVPage.js
+++ frontend/client/src/containers/CreateCVPage.js
@@ -14,74 +14,60 @@ import { setMessages } from "../features/user";
 
 const CreateCVPage = (props) => {
   const navigate = useNavigate();
-  const [showSummaryForm, setShowSummaryForm] = useState(false);
   const dispatch = useDispatch();
 
-  const [experiences, setExperiences] = useState([]);
   const [experienceForm, setExperienceForm] = useState(false);
   const { isAuthenticated } = useSelector((state) => state.user);
 
+  const [displayBasic, setDisplayBasic] = useState(false);
+  const [displayModal, setDisplayModal] = useState(false);
+  const [displayMaximizable, setDisplayMaximizable] = useState(false);
+  const [displayPosition, setDisplayPosition] = useState(false);
+  const [displayResponsive, setDisplayResponsive] = useState(false);
+  const [position, setPosition] = useState('center');
 
+  // Dialog functions for pop up window to delete CV
+  const dialogFuncMap = {
+      'displayBasic': setDisplayBasic,
+      'displayModal': setDisplayModal,
+      'displayMaximizable': setDisplayMaximizable,
+      'displayPosition': setDisplayPosition,
+      'displayResponsive': setDisplayResponsive
+  }
 
-    const [displayBasic, setDisplayBasic] = useState(false);
-    const [displayModal, setDisplayModal] = useState(false);
-    const [displayMaximizable, setDisplayMaximizable] = useState(false);
-    const [displayPosition, setDisplayPosition] = useState(false);
-    const [displayResponsive, setDisplayResponsive] = useState(false);
-    const [position, setPosition] = useState('center');
+  const onClickPopUp = (name, position) => {
+      dialogFuncMap[`${name}`](true);
 
-    const dialogFuncMap = {
-        'displayBasic': setDisplayBasic,
-        'displayModal': setDisplayModal,
-        'displayMaximizable': setDisplayMaximizable,
-        'displayPosition': setDisplayPosition,
-        'displayResponsive': setDisplayResponsive
-    }
+      if (position) {
+          setPosition(position);
+      }
+  }
 
-    const onClickPopUp = (name, position) => {
-        dialogFuncMap[`${name}`](true);
+  const onHide = (name) => {
+      dialogFuncMap[`${name}`](false);
+  }
 
-        if (position) {
-            setPosition(position);
-        }
-    }
+  const renderFooter = (name) => {
+    return (
+      <div>
+        <Button
+          label="No"
+          icon="pi pi-times"
+          onClick={() => onHide(name)}
+          className="p-button-text" />
+        <Button
+          label="Yes"
+          icon="pi pi-check"
+          onClick={() => {
+            onHide(name);
+            deleteCV();
+          }}
+          autoFocus />
+      </div>
+    );
+  }
 
-    const onHide = (name) => {
-        dialogFuncMap[`${name}`](false);
-    }
-
-    const renderFooter = (name) => {
-      return (
-        <div>
-          <Button
-            label="No"
-            icon="pi pi-times"
-            onClick={() => onHide(name)}
-            className="p-button-text" />
-          <Button
-            label="Yes"
-            icon="pi pi-check"
-            onClick={() => {
-              onHide(name);
-              deleteCV();
-            }}
-            autoFocus />
-        </div>
-      );
-    }
-
-
-  const cvId = useParams().id;
-  console.log(cvId);
-
-  const getCVDetails = async () => {
-    console.log(window.location.origin);
-    const res = axios.get(`cvs/?id=${cvId}`);
-
-    const data = await res.data;
-    console.log(data);
-  };
-
+  // Check if user is authenticated
   useEffect(() => {
     if (!isAuthenticated) {
       navigate("/login");
@@ -94,6 +80,20 @@ const CreateCVPage = (props) => {
     cvId: cvId,
   }
 
+  // Get CV id from url
+  const cvId = useParams().id;
+  console.log(cvId);
+
+  // Get CV details
+  const getCVDetails = async () => {
+    console.log(window.location.origin);
+    const res = axios.get(`cvs/?id=${cvId}`);
+
+    const data = await res.data;
+    console.log(data);
+  };
+
+  // Delete CV
   const deleteCV = async () => {
     try {
       await axios.delete(`cvs/?id=${cvId}`);
@@ -103,11 +103,6 @@ const CreateCVPage = (props) => {
       console.log("Error deleting CV");
     }
   };
-
-
-  console.log(IdProps);
-
-
 
   return (
     <Layout title='CV-builder | My CVs' content='Welcome to the Create CV page'>
