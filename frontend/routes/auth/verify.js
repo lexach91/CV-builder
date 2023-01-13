7aa163f8a5817d04f0f3e553131f7a6e89a08f60
lexach91 <ronmctailor@protonmail.com>
2023-01-13 15:55:36 +0700

    Rewrite verify route to use axios instead of fetch

--- frontend/routes/auth/verify.js
+++ frontend/routes/auth/verify.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = (...args) =>
 	import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
+const axios = require('axios');
 const router = express.Router();
 console.log('in verify route');
 
@@ -9,19 +9,28 @@ console.log('in verify route');
 router.get('/api/auth/verify', async (req, res) => {
   // parse access token from cookie
 	const { access } = req.cookies;
+	// console.log(access);
+	console.log(req.cookies);
+	// res.cookie('access', access)
+	// res.cookie('refresh', req.cookies.refresh)
 
 //   const body = JSON.stringify({
 //     token: access,
 //   });
 
 	try {
-		const apiRes = await fetch(`${process.env.API_URL}/api/auth/verify`, {
-			method: 'GET',
-			headers: {
-				Accept: 'application/json',
-				Authorization: `Bearer ${access}`,
-			},
-		});
+		// const apiRes = await fetch(`${process.env.API_URL}/api/auth/verify`, {
+		// 	method: 'GET',
+		// 	headers: {
+		// 		Accept: 'application/json',
+		// 		Authorization: `Bearer ${access}`,
+		// 		'Cookie': "lalala"
+		// 	},
+		// 	credentials: 'include',
+		// });
+		const apiRes = await axios.get('auth/verify',{}, {withCredentials: true});
+		console.log(apiRes);
+
 		// in return we get only a response code
 		return res.status(apiRes.status).json({});
 
@@ -30,7 +39,7 @@ router.get('/api/auth/verify', async (req, res) => {
 		// return res.status(apiRes.status).json(data);
 	} catch (err) {
 		return res.status(500).json({
-			error: 'Something went wrong when verifying token',
+			error: err,
 		});
 	}
 });
